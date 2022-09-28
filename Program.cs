129323881db6ef1a67a77032da9236b8ec2928c4
Yan Martins Esteves <yan.esteves@audaces.com>
2022-09-28 00:28:35 -0300

    Adicionando configurações de nome e descrições para as operações das queries.

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ builder.Services
     .AddQueryType()
         .AddTypeExtension<CharacterQueries>()
         .AddTypeExtension<ReviewQueries>()
-        .AddTypeExtension<DeveloperQueries>()
+        .AddTypeExtension<DeveloperQueries>() // adicionando uma nova anotação para queries
         // .AddMutationType()
         // .AddTypeExtension<ReviewMutations>()
         // .AddSubscriptionType()
@@ -25,7 +25,8 @@ builder.Services
     .AddType<Human>()
     .AddType<Droid>()
     .AddType<Starship>()
-    .AddType<DeveloperQueries>()
+    .AddType<DeveloperQueries>() // adiciono um novo tipo de dado a ser retornado.
+
     .AddTypeExtension<CharacterResolvers>()
 
     .AddFiltering()
@@ -42,15 +43,20 @@ app.Run();
 [ExtendObjectType(OperationTypeNames.Query)]
 public class DeveloperQueries
 {
+    [GraphQLName("api_info")] // alterando nome da operação da query.
+    [GraphQLDescription("Função para retornar dados do autor da api.")] // definindo uma descrição para a operação.
     public Developer GetApi_Info()
     {
         return new Developer { Author  = "Yan Esteves", Github = "https://github.com/yanestevesufjf", Course = "DevInHouse" };
     }
 }
-
+[GraphQLDescription("Um objeto do tipo Developer")]
 public class Developer
 {
+    [GraphQLDescription("Nome do responsável pela API.")]
     public string? Author {get; set;}
+    [GraphQLDescription("Github do desenvolvedor.")]
     public string? Github {get;set;}
+    [GraphQLDescription("Nome do curso que foi aplicado este conteúdo.")]
     public string? Course {get;set;}
 }
